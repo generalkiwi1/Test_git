36e903793d13cd9b2ad4fcba2e0d1c063c9e65ae
generalkiwi1 <xx-mik-xx@hotmail.com>
2018-04-26 14:57:51 -0400

    Stached

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 
 int main(int argc, char* argv)
 {
+<<<<<<< Updated upstream
+=======
+
+	/// Alllllllllllllllllllllllllllll
+>>>>>>> Stashed changes
 	std::cout << "Hello world" << std::endl;
 
 	
